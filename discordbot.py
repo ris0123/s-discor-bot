b68c75da549dc879ae8d232b1bc5400ecd8f4053
ris0123 <64739787+ris0123@users.noreply.github.com>
2020-05-15 23:59:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,7 @@
 import discord
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'NzEwODExMzU4NzA2NTMyMzcy.Xr6FkQ.Hi4Fco2Ds-kumStkMEsUq91y1tk'
-
+TOKEN = 'NzEwODExMzU4NzA2NTMyMzcy.Xr6rtw.X4AjP56q5OmA5k9RW_xdNQOwT2Q'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -24,35 +23,26 @@ async def on_message(message):
     if message.content == 'おはよう':
         await message.channel.send('今日も一日頑張ろう！')
 
-    if message.author.bot:
-        return
     if message.content == 'おやすみ':
         await message.channel.send('ゆっくり休んでね')
 
-    if message.author.bot:
-        return
     if message.content == 'ただいま':
         await message.channel.send('おかえり')
 
-    if message.author.bot:
-        return
     if message.content == '疲れた':
         await message.channel.send('疲れた時は休もうね')
 
-    if message.author.bot:
-        return
     if message.content == '大好き':
         await message.channel.send('ありがとう')
 
-    if message.author.bot:
-        return
     if message.content == 'いってきます':
         await message.channel.send('いってらっしゃい')
 
-    if message.author.bot:
-        return
     if message.content == '行ってきます':
         await message.channel.send('いってらっしゃい')
-        
-    # Botの起動とDiscordサーバーへの接続
+
+    if message.content == '眠い':
+        await message.channel.send('はよ寝ろ')
+
+# Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
